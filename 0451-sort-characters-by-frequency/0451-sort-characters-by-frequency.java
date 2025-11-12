e73157923508daeba11d38c3a31467317ca7a1fd
anshumann01 <155174498+anshumann01@users.noreply.github.com>
2025-11-12 15:04:52 +0530

    Update 0451-sort-characters-by-frequency.java

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
@@ -7,7 +7,7 @@ class Solution {
             map.put(s.charAt(i), map.getOrDefault(s.charAt(i), 0) + 1);
         }
          List<Map.Entry<Character, Integer>> list = new ArrayList<>(map.entrySet());
-        list.sort((a, b) -> b.getValue().compareTo(a.getValue())); //Sorted the Map in descending order of Frequency
+        list.sort((a, b) -> b.getValue().compareTo(a.getValue())); //Sorted the list in descending order of Frequency
         StringBuilder sb = new StringBuilder();
         for(int i=0;i<list.size();i++) {
             for(int j=0;j<list.get(i).getValue();j++){
