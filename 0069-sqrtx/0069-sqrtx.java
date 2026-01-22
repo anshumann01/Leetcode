eb7883900193121d291c9f2ada33076503b70f88
Anshuman Gupta <155174498+anshumann01@users.noreply.github.com>
2026-01-22 09:13:09 +0530

    Time: 1 ms (96.54%), Space: 41.5 MB (99.96%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -1,15 +1,19 @@
 class Solution {
     public int mySqrt(int x) {
-        int sqrt=0;
-        if(x==0 || x==1) return x;
-        for(int i=0;i<x;i++) {
-            if((long)i*i<=x) {
-                sqrt=i;
-            }
-            else {
-                break;
-            }
+       long start=0;
+       long end=x;
+       while(start<=end) {
+        long mid=start+(end-start)/2;
+        if((mid*mid)==x) {
+            return (int)mid;
         }
-        return sqrt;
+        else if((mid*mid)>x) {
+            end=mid-1;
+        }
+        else {
+            start=mid+1;
+        }
+       }
+       return (int)end;
     }
 }
