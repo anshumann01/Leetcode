13a348d00fd5978b3ce3cfec9df413076aec424c
Anshuman Gupta <155174498+anshumann01@users.noreply.github.com>
2026-03-11 13:18:32 +0530

    Time: 3 ms (6.69%), Space: 42.2 MB (50.09%) - LeetHub

---
+++ 1009-complement-of-base-10-integer/1009-complement-of-base-10-integer.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int bitwiseComplement(int n) {
+        String binary = Integer.toBinaryString(n);
+        String complement = "";
+        for(int i=0;i<binary.length();i++) {
+            if(binary.charAt(i)=='0') {
+                complement+='1';
+            }
+            else {
+                complement+='0';
+            }
+        }
+        int number = Integer.parseInt(complement,2);
+        return number;
+    }
+}
