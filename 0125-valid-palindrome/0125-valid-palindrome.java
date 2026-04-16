3c4f02233e367e2e87da89b0e0feadcc4bda86d2
Anshuman Gupta <155174498+anshumann01@users.noreply.github.com>
2026-04-16 21:51:35 +0530

    Time: 17 ms (11.3%), Space: 47.5 MB (11.89%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        s = s.replaceAll("[^a-zA-Z0-9]","").replaceAll(" ","");
+        s=s.toLowerCase();
+        for(int i=0;i<s.length();i++) {
+            if(s.charAt(i)!=s.charAt(s.length()-i-1)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
