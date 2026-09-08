8ebadd51454e00d8a07c2fa7fe90e135e2618824
Anshuman Gupta <155174498+anshumann01@users.noreply.github.com>
2026-09-08 20:49:12 +0530

    Time: 0 ms (100%), Space: 43.6 MB (40.73%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,5 +1,26 @@
 class Solution {
-    public void sortColors(int[] nums) {
-        Arrays.sort(nums);
+    public void sortColors(int[] nums) { //Dutch National Flag Algo
+        int lo =0;
+        int mid =0;
+        int hi = nums.length-1;
+        int temp=0;
+        while(mid<=hi) {
+            if(nums[mid]==0) {
+                temp = nums[lo];
+                nums[lo] = nums[mid];
+                nums[mid]=temp;
+                lo++;
+                mid++;
+            }
+            else if(nums[mid]==1) {
+                mid++;
+            }
+            else {
+                temp = nums[mid];
+                nums[mid] = nums[hi];
+                nums[hi] = temp;
+                hi--;
+            }
+        }
     }
 }
