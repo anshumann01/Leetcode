0a3d0d5854588999c3a28869aa63f6234ae6f2cc
Anshuman Gupta <155174498+anshumann01@users.noreply.github.com>
2026-09-11 21:52:34 +0530

    Time: 31 ms (81.8%), Space: 43.5 MB (96.7%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -4,15 +4,15 @@ class Solution {
         for(int num:nums) {
             total+=num;
         }
-        int target = total/2;
         if(total%2!=0) {
             return false;
         }
+        int target = total/2;
         boolean dp[] = new boolean[target+1];
         dp[0]=true;
         for(int num:nums) {
-            for(int i=target;i>=num;i--) {
-                dp[i] = dp[i] || dp[i-num];
+            for(int j=target;j>=num;j--) {
+                dp[j] = dp[j] || dp[j-num];
             }
         }
         return dp[target];
